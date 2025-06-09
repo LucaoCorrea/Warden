08cf70e6e00073f6ddcc397d39b1c4ec4c5a53db
LucaoCorrea <lucascorrea1505@gmail.com>
2025-06-09 11:05:39 -0300

    Add: Datatable for table-contacts

--- Warden/Migrations/20250609133042_migrations.Designer.cs
+++ Warden/Migrations/20250609133042_migrations.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warden.Data;
 
@@ -10,9 +11,11 @@ using Warden.Data;
 namespace Warden.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250609133042_migrations")]
+    partial class migrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Warden/wwwroot/js/site.js
+++ Warden/wwwroot/js/site.js
@@ -1,3 +1,6 @@
-﻿$('.close-alert').click(function () {
-    $(this).closest('.alert').hide(); // Fecha apenas o alerta correspondente
+﻿let table = new DataTable('#table-contacts'); //optei por não traduzir
+
+$('.close-alert').click(function () {
+        $(this).closest('.alert').hide(); // Fecha apenas o alerta correspondente
 });
+
